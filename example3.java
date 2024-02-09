66475c39227714dfce15f2cce08b3701970c41e4
izzyara <izzybzb@gmail.com>
2024-02-09 14:32:56 -0500

    Coding Problems 2

---
+++ example3.java
@@ -0,0 +1,7 @@
+public class example3
+{
+        public static void main (String[] args)
+    {
+    System.out.println("Anderson\t3452\nBoyton\t\t5344\nCarrington\t3677\nHouston\t\t5988\nMcDougal\t5233\nRobertson\t3632\nStryker\t\t5112");
+    }
+}
